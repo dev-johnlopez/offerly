ffe0c0efa4caddfabeb9b4e73ed35ae9c750df77
John Lopez <johnlopez@Johns-MacBook-Pro.local>
2019-10-06 14:47:24 -0500

    update

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: flask db upgrade; gunicorn offerly:app
 admin: flask users create john@johnlopez.org --password some-dummy-password --active
 #worker: rq worker -u $REDIS_URL offerly-tasks
+user_seed: python add_user.py
--- add_user.py
+++ add_user.py
@@ -1,9 +1,10 @@
-from app import db
+from app import create_app, db, cli
 from flask import current_app
 from flask_security import Security, SQLAlchemyUserDatastore, current_user
 from flask_security.utils import encrypt_password
 from app.auth.models import Role, User
 # Setup Flask-Security
+app = create_app()
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 user_datastore.create_user(email=os.environ.get('ADMIN_USERNAME'), password=encrypt_password(os.environ.get('ADMIN_PASSWORD')))
